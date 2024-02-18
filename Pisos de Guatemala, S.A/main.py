b0c61d700a24daac86da484d9967fac5ca2ac3bf
Angel Guillermo de Jesús Pérez Jiménez <140100841+LightDemon12@users.noreply.github.com>
2024-02-18 12:56:43 -0600

    Archivos y Interfaz de usuario básica
    Creación de archivos para uso posterior del proyecto e interfaz básica

---
+++ Pisos de Guatemala, S.A/main.py
@@ -0,0 +1,23 @@
+
+def main ():
+    print("Bienvenido a Pisos de Guatemala, S.A.")
+    print("=======================================")
+    while True:
+       print("Menú principal") 
+       print("1. Mostrar todos los pisos disponibles")
+       print("2. Seleccionar un piso")
+       print("3. Salir")
+
+       opcion = int(input("Ingrese la opción que desea: "))
+       if opcion == 1:
+           print("Mostrar todos los pisos disponibles")
+       elif opcion == 2:
+           print("Seleccionar un piso:")      
+       elif opcion == 3:
+            print("¡Hasta luego!")      
+            break
+       else:
+            print("Opción inválida")
+   
+if __name__ == "__main__":
+   main()
